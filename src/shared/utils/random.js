0e6d4836f61660c13cf1a211756f702a0f4f18ac
Soulkai <kaiurysouza55@gmail.com>
2026-04-25 02:03:08 -0400

    Update random.js

--- src/shared/utils/random.js
+++ src/shared/utils/random.js
@@ -1,22 +1,23 @@
-// src/shared/utils/random.js
+// Função mágica que escolhe um item baseado no "peso" (probabilidade) de cada um
+function weightedRandom(options) {
+    let totalWeight = 0;
+    
+    // Soma todos os pesos
+    for (const key in options) {
+        totalWeight += options[key];
+    }
 
-/**
- * Função mágica para rolar o destino baseado nas chances (pesos)
- * @param {Array} items - Lista de objetos contendo uma propriedade 'chance'
- */
-function rollWeighted(items) {
-    const totalWeight = items.reduce((sum, item) => sum + item.chance, 0);
+    // Gira a roleta
     let randomNum = Math.random() * totalWeight;
 
-    for (const item of items) {
-        if (randomNum < item.chance) {
-            // Retorna o item inteiro sorteado
-            return item;
+    // Define o vencedor
+    for (const key in options) {
+        if (randomNum < options[key]) {
+            return key;
         }
-        randomNum -= item.chance;
+        randomNum -= options[key];
     }
-    // Retorno de segurança (failsafe)
-    return items[items.length - 1];
 }
 
-module.exports = { rollWeighted };
+// Exportando a técnica para que as Leis do Karma possam usá-la
+module.exports = { weightedRandom };
